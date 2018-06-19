92cad87f8b6078e5abcd72b371ae1f0c554c4191
RomanDerkach <derkachr@gmail.com>
2018-06-19 18:15:58 +0300

    Templates added, JINJA work

--- app/routers.py
+++ app/routers.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello my dear friend"
+    user = {'username': 'Roman'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html", posts=posts, user=user)
