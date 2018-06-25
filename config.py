0dddc619dca9f3475def38520ca15cd103006ce9
RomanDerkach <derkachr@gmail.com>
2018-06-25 16:46:15 +0300

    feature/4: database connection, migration and models

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "standart-secret-key-1111"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'standart-secret-key-1111'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABSE_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
