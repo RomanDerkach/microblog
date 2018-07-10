df0460245e8f16f6d5e4a4636c483be4d1c9455b
RomanDerkach <derkachr@gmail.com>
2018-07-10 17:20:40 +0300

    feature/5 user login/logout functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
@@ -8,6 +9,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 # bottom import to avoid circular imports
 from app import routers, models
